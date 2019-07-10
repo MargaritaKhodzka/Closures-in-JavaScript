a4ab019afbdf2f04fa7211b3f201f5fd4f315e79
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-10 16:16:45 -0400

    closure explained

--- V2/closure.js
+++ V2/closure.js
@@ -2,4 +2,18 @@ var birds = 3;
 
 function dogHouse() {
 	var dogs = 8;
+	// console.log(birds); // 3
+	// console.log(dogs); // 8
+
+ 	// inner function. Has access to the outer function scope
+	function showDogs() {
+		console.log(dogs);
+	}
+	return showDogs();
 }
+
+var getDogs = dogHouse();
+getDogs;
+
+// console.log(birds); // 3
+// console.log(dogs); // ReferenceError
