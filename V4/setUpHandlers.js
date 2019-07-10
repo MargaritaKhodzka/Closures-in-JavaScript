bbd47270ead712f86bed3e4027c2077ec74e3d43
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-10 16:42:43 -0400

    create block scope with let

--- V4/setUpHandlers.js
+++ V4/setUpHandlers.js
@@ -1,9 +1,9 @@
-var buttons = document.getElementsByTagName('button');
+let buttons = document.getElementsByTagName('button');
 
-for(var i = 0; i < buttons.length; i += 1) {
-	var button = buttons[i];
-	var buttonName = button.innerHTML;
-	button.addEventListener('click', function() {
+for(let i = 0; i < buttons.length; i++) {
+	let button = buttons[i];
+	let buttonName = button.innerHTML;
+	button.addEventListener('click', () => {
 		console.log(buttonName);
 	});
 }
